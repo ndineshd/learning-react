4a42ef7c4d34831a6acf245731d450acb41a42dd
ndineshd <ndineshb@northladder.com>
2023-10-03 18:48:33 +0530

    feat: Add function component to add and remove remove eventlistiner
    -Added functional components to initialize the x and y
    -Added logMousePosition to update the mouse position values
    -Added useEffect to addeventlistener and returned to remove eventlistener

---
+++ src/ReactHooks/components/FunctionMouse.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+
+function FunctionMouse() {
+
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+
+    const logMousePosition = e => {
+        setX(e.clientX)
+        setY(e.clientY)
+    }
+
+    useEffect(() => {
+        window.addEventListener('mousemove', logMousePosition)
+
+        return() => {
+          window.removeEventListener('mousemove', logMousePosition)
+        }
+    }, [])
+
+  return (
+    <div>
+        <h3>UseEffect Mouse using function</h3>
+      Hooks X - {x} Y - {y}
+    </div>
+  )
+}
+
+export default FunctionMouse
