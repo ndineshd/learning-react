06dcdae5886fcfc4809d2c987606e500e502c777
ndineshd <ndineshb@northladder.com>
2023-10-03 18:27:42 +0530

    feat: Add components and import components
    -Added components of UseEffect, UseEffectFunction, UseEffectMouse, FunctionMouse and MouseContainer
    -Imported all the components
    -Modified h2 to h1 tag

--- src/ReactHooks/ReactHooks.js
+++ src/ReactHooks/ReactHooks.js
@@ -4,16 +4,29 @@ import HookCounter from './components/HookCounter'
 import HookPreState from './components/HookPreState'
 import ObjectUseState from './components/ObjectUseState'
 import ArrayUseState from './components/ArrayUseState'
+import UseEffect from './components/UseEffect'
+import UseEffectFunction from './components/UseEffectFunction'
+import UseEffectMouse from './components/UseEffectMouse'
+import FunctionMouse from './components/FunctionMouse'
+import MouseContainer from './components/MouseContainer'
 
 function ReactHooks() {
   return (
     <div>
-        <h2>useState Hook</h2>
+        <h1>useState Hook</h1>
+
      <UseState />
      <HookCounter />
      <HookPreState />
      <ObjectUseState />
      <ArrayUseState />
+     <h1>UseEffect Hook</h1>
+     <UseEffect />
+     <UseEffectFunction />
+     <UseEffectMouse />
+     <FunctionMouse />
+     <MouseContainer />
+
     </div>
   )
 }
