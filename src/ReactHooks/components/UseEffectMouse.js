4f92ce4cbabdcdee8d90f6a86f1225f5ece34490
ndineshd <ndineshb@northladder.com>
2023-10-03 18:43:54 +0530

    feat: Add component to print mouse move position
    -Added class component with constructor to define x and y initialised to 0.
    -Added logMousePosition and componentDidMount to update the x and y position values

---
+++ src/ReactHooks/components/UseEffectMouse.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class UseEffectMouse extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         x: 0,
+         y: 0
+      }
+    }
+
+    logMousePosition = e => {
+        this.setState({x: e.clientX, y: e.clientY})
+    }
+
+    componentDidMount() {
+        window.addEventListener('mousemove', this.logMousePosition)
+    }
+    
+  render() {
+    return (
+      <div>
+        <h3>UseEffect Mouse using class </h3>
+        X - {this.state.x} Y - {this.state.y}
+      </div>
+    )
+  }
+}
+
+export default UseEffectMouse
