63ccfc5890e45946b74d7b50b31b823682a0af3e
ndineshd <ndineshb@northladder.com>
2023-10-04 17:12:25 +0530

    refactor: Remove inline function from buttons

--- src/ReactHooks/components/ObjectUseState.js
+++ src/ReactHooks/components/ObjectUseState.js
@@ -1,24 +1,32 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 function ObjectUseState() {
 
     const [name, setName] = useState({firstName:'', lastName:''})
+
+    const inputFirstName = e => {
+      setName({...name, firstName: e.target.value}) //...name used to spread
+    }
+
+    const inputLastName = e => {
+      setName({...name, lastName: e.target.value})
+    }
+
   return (
-
     <>
-    <h3>UseState with object</h3>
+      <h3>UseState with object</h3>
 
-    <form>
+      <form>
 
-        <input type='text' value={name.firstName} onChange={e => setName({...name, firstName: e.target.value})} />
+          <input type='text' value={name.firstName} onChange={inputFirstName} />
 
-        <input type='text' value={name.lastName} onChange={e => setName({...name, lastName: e.target.value})} />
+          <input type='text' value={name.lastName} onChange={inputLastName} />
 
-        <p>Your first name is : <strong>{name.firstName}</strong></p>
+          <p>Your first name is : <strong>{name.firstName}</strong></p>
 
-        <p>Your last name is : <strong>{name.lastName}</strong></p>
+          <p>Your last name is : <strong>{name.lastName}</strong></p>
 
-    </form>
+      </form>
     </>
     
   )
