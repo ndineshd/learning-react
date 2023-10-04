dad3ef8085e17ec96b980b75168c4f660ac50b2b
ndineshd <ndineshb@northladder.com>
2023-10-04 16:59:46 +0530

    refactor: Remove inline function from button and  'import React'
    -Removed the inline function from the button component
    -Removed the 'import React'

--- src/ReactHooks/components/HookCounter.js
+++ src/ReactHooks/components/HookCounter.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 function HookCounter() {
-    const [count, setCount] = useState(0 )
+    const [count, setCount] = useState(0)
+
+    const HandleCount = () => {
+      setCount(count + 1)
+    }
+
   return (
     <div>
       <h3>using usestate in function</h3>
-      <button onClick={() => setCount(count + 1)} title='Click to increase the value'>Count {count}</button>
+      <button onClick={HandleCount} title='Click to increase the value'>Count {count}</button>
     </div>
   )
 }
