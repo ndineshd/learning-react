0417dff16d1ca26746a970c4815dac60bdae4c57
ndineshd <ndineshb@northladder.com>
2023-10-04 17:11:13 +0530

    refactor: Remove inline function from button, initialized value
    - Removed inline functions from button
    - Initialized direct value with 'useState'

--- src/ReactHooks/components/HookPreState.js
+++ src/ReactHooks/components/HookPreState.js
@@ -1,22 +1,32 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 function HookPreState() {
-    const initialCount =0
-    const [count, setCount] = useState(initialCount)
+    const [count, setCount] = useState(0)
 
-    const incrementFive = () => {
-      for(let i=0; i<5; i++)
+    const handleIncrementFive = () => {
+      setCount(count + 5)
+    }
+
+    const handleReset = () => {
+      setCount(0)
+    }
+
+    const handleIncrement = () => {
       setCount(prevCount => prevCount + 1)
+    }
+
+    const handleDecrement = () => {
+      setCount(prevCount => prevCount - 1)
     }
 
   return (
     <div>
         <h3>Hook Pre State using useState</h3>
         <p>count: {count}</p>
-        <button onClick={() => setCount(initialCount)} title='Click to reset the value'>Reset</button>
-        <button onClick={() => setCount(prevCount => prevCount + 1)} title='click to increase the value'>Increment</button>
-        <button onClick={() => setCount(prevCount => prevCount - 1)} title='click to decrease the value'>Decrement</button>
-        <button onClick={incrementFive} title='Click to increase 5 times'>Increment +5</button>
+        <button onClick={handleReset} title='Click to reset the value'>Reset</button>
+        <button onClick={handleIncrement} title='click to increase the value'>Increment</button>
+        <button onClick={handleDecrement} title='click to decrease the value'>Decrement</button>
+        <button onClick={handleIncrementFive} title='Click to increase 5 times'>Increment +5</button>
       
     </div>
   )
